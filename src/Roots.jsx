d45997b8c03094c1164cff82f02581d153f0a8c6
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-04-08 16:21:54 +0600

    SubNavber create and style

---
+++ src/Roots.jsx
@@ -0,0 +1,13 @@
+import SubHeading from './components/SubHeading';
+import './index.css'
+
+const Roots = () => {
+    return (
+        <div className='bgImg h-screen '>
+            <SubHeading/>
+            <h2 className='text-white saira'>Roots file</h2>
+        </div>
+    );
+};
+
+export default Roots;
---
+++ src/components/SubHeading.jsx
@@ -0,0 +1,21 @@
+import './../fonts/fonts.css'
+import { SlLocationPin } from "react-icons/sl";
+import { MdOutlineMail } from "react-icons/md";
+import { FaUserEdit } from "react-icons/fa";
+
+const SubHeading = () => {
+    return (
+        <div className="w-11/12 mx-auto textColor">
+            <div className="flex justify-between items-center pt-5 pb-2 px-2 lato font-light">
+                <div className='flex gap-20'>
+                    <h2 className='flex items-center gap-2'><SlLocationPin />2150-Nakla ,Sherpur,Mymensingh</h2>
+                    <h2 className='flex items-center gap-2'><MdOutlineMail />shafiqulislamsagor277@gmail.com</h2>
+                    <h2 className='flex items-center gap-2'><FaUserEdit/>Shafiqul Islam Sagor</h2>
+                </div>
+            </div>
+            <div className="border-b mt-3 opacity-60"></div>
+        </div>
+    );
+};
+
+export default SubHeading;
